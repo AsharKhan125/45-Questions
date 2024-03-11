a79c20ca4b48a394d5574c1a2ddb082fb0a359fb
AsharKhan125 <160245079+AsharKhan125@users.noreply.github.com>
2024-03-12 03:06:35 +0500

    Add files via upload

---
+++ Q4/main.js
@@ -0,0 +1,3 @@
+var famous_person = "Albert Einstein";
+var message = "".concat(famous_person, " once said, \"A peron who never made mistakes never tried anyrhing new\"");
+console.log(message);
---
+++ Q9/main.js
@@ -0,0 +1,4 @@
+//Ashar Khan 05.02.1995
+//Coding Practice 100days Coding
+var myName = "Ashar Khan";
+console.log("Hello ".concat(myName, ", would you like to read some Typescript today?"));
